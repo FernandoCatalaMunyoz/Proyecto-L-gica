22e3602a85648c6f0f6afcda4ab9cbafe16e4919
Fernando <fernandocatalamunyoz@gmail.com>
2024-02-12 17:58:53 +0100

    codigo hecho a falta de readme

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+export const fn = (a, b, c) => {
+  for (let i = 0; i < 40; i++) {
+    if (a + b + c > 40) {
+      throwerror();
+    }
+    if ((a + b + c) % 3 !== 0) {
+      throwerror();
+    }
+    if (a < 0 || b < 0 || c < 0) {
+      throwerror();
+    }
+    return Math.ceil((12 * a + 14 * b + 16 * c) / 3);
+  }
 };
